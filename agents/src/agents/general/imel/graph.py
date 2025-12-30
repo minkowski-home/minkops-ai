e2e8f527576fab92b3a0f9099f12aaebacd9e501
Kartik Kumar <kartikthory@icloud.com>
2025-12-30 20:23:05 +0530

    Kall agent graph, nodes, policy, prompts, state and tools initialized

--- agents/src/agents/general/imel/graph.py
+++ agents/src/agents/general/imel/graph.py
@@ -1,14 +1,4 @@
-from typing import Annotated, TypedDict, Literal
-from langgraph.graph import StateGraph, START, END  # type: ignore
 from langchain_ollama import ChatOllama
-from langgraph.types import Command
-from langchain_core.messages import HumanMessage
-import os
-import dotenv
-
-dotenv.load_dotenv()
-
-
 
 # Initialize Gemma 3 via Ollama
 gemma = ChatOllama(model="gemma3:4b", 
